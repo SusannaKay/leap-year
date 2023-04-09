1a501c04272fdc66ef93b29d6e513fef01e24d71
Susanna Kayed <105354050+SusannaKay@users.noreply.github.com>
2023-04-09 20:18:58 +0200

    def function + days in month check
    I added two functions: one check if the year is leap and the other, based on the return of the first, tells how many days in the month we have

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
-year = int(input("Which year do you want to check? "))
-if year % 4 == 0:
+def is_leap(year):
+  if year % 4 == 0:
     if year % 100 == 0:
-        if year % 400 == 0:
-            print("Leap year.")
-        else:
-            print("Not leap year.")
+      if year % 400 == 0:
+        return True
+      else:
+        return False
     else:
-        print("Leap year.")
-else:
-    print ("Not leap year.")
+      return True
+  else:
+    return False
+
+def days_in_month(year, month):
+  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  if is_leap(year) and month == 2:
+      return 29
+  else:
+      days = month_days[month-1]
+      return days    
+  
+  
+
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
+
+
+
+
+
+
 
